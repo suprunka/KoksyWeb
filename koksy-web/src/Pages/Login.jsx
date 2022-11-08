111898326e49944bac8664747d3590d4ac1ad09b
Patrycja <psu@norlysenergytrading.com>
2022-11-08 21:47:33 +0100

    improvements

--- koksy-web/src/Pages/Login.jsx
+++ koksy-web/src/Pages/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
   useEffect(() => {
     if(token)
     navigate('/workouts')
-  },[]); 
+  },[token]); 
    const login=()=> {
     dispatch(loginAction({email, password}))
   }
